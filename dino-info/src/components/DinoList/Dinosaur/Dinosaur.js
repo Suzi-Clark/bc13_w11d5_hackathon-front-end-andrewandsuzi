9fa58843ff3e3e29b689d2a4282d931181c1b85c
(Suzi) Susanna Clark <100801053+Suzi-Clark@users.noreply.github.com>
2022-12-09 16:11:34 +0000

    updated dinosaur js for image toggle

--- dino-info/src/components/DinoList/Dinosaur/Dinosaur.js
+++ dino-info/src/components/DinoList/Dinosaur/Dinosaur.js
@@ -2,11 +2,23 @@ import React from 'react';
 import Description from './Description/Description';
 
 function Dinosaur({ info }) {
+  let descTog = false;
+  function handleClick() {
+    console.log('clicked');
+    descTog = !descTog;
+    console.log(descTog);
+  }
+
   return (
     <div className='container'>
       <p className='name'>{info.dino_name}</p>
-      <img src={info.image_url} alt='Cool dino' height='200px'></img>
-      <Description furtherInfo={info} />
+      <img
+        src={info.image_url}
+        alt='Cool dino'
+        height='200px'
+        onClick={() => handleClick()}
+      ></img>
+      {descTog ? true : <Description furtherInfo={info} />}
     </div>
   );
 }
