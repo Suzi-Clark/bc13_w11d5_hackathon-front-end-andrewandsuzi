81870e413399a8a859695cab3ca4b966425baaaa
Andrew <andrew@eseeds.com>
2022-12-09 15:50:47 +0000

    data

--- dino-info/src/components/DinoList/Dinosaur/Description/Description.js
+++ dino-info/src/components/DinoList/Dinosaur/Description/Description.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './description.css';
 
-function Description() {
+function Description({ furtherInfo }) {
   return (
     <div>
-      <p className='heading'>Scientific Name:</p>
-      <p>Data from api</p>
-      <p className='heading'>Description:</p>
-      <p className='heading'>Size:</p>
-      <p className='heading'>Located:</p>
+      <p className='heading'>
+        <b>Scientific Name:</b> {furtherInfo.scientific_name}
+      </p>
+      <p className='heading'>
+        <b>Description:</b> {furtherInfo.description}
+      </p>
+      <p className='heading'>
+        <b>Size:</b> {furtherInfo.size}
+      </p>
+      <p className='heading'>
+        <b>Located:</b> {furtherInfo.location}
+      </p>
     </div>
   );
 }
--- dino-info/src/components/DinoList/Dinosaur/Dinosaur.js
+++ dino-info/src/components/DinoList/Dinosaur/Dinosaur.js
@@ -4,9 +4,9 @@ import Description from './Description/Description';
 function Dinosaur({ info }) {
   return (
     <div className='container'>
-      <p>{info.dino_name}</p>
+      <p className='name'>{info.dino_name}</p>
       <img src={info.image_url} alt='Cool dino' height='100px'></img>
-      <Description />
+      <Description furtherInfo={info} />
     </div>
   );
 }
